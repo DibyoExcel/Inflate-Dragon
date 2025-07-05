cf9e743acccf50d5425881afee7bce6a365ad32c
DubEnderDragon <dibyo.excel.90@gmail.com>
2025-07-05 20:56:04 +0700

    uhhhh

--- Inflation Dragon.js
+++ Inflation Dragon.js
@@ -915,7 +915,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "98";
+	app.meta.h["build"] = "99";
 	app.meta.h["company"] = "DubEnderDragon";
 	app.meta.h["file"] = "Inflation Dragon";
 	app.meta.h["name"] = "Inflation Dragon";
@@ -70189,7 +70189,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 798239;
+	this.version = 345632;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
